f2e0fde993c4eef08efefca156b05f61293f85d7
KangHyunChan <kanghyun122@naver.com>
2026-05-17 13:09:37 +0900

    Add product repository

---
+++ src/main/java/com/myproject/ecommerce_service/repository/product/JdbcProductRepository.java
@@ -0,0 +1,70 @@
+package com.myproject.ecommerce_service.repository.product;
+
+import com.myproject.ecommerce_service.domain.product.Product;
+import com.myproject.ecommerce_service.domain.product.ProductStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcProductRepository implements ProductRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(
+            rs.getLong("product_id"),
+            rs.getString("product_name"),
+            rs.getInt("price"),
+            rs.getInt("quantity"),
+            rs.getString("description"),
+            rs.getString("imageURL"),
+            ProductStatus.valueOf(rs.getString("status"))
+    );
+
+
+    @Override
+    public Product registration(Product product) {
+        String sql = "INSERT INTO product (product_name, price, quantity, description, imageURL, status) VALUES(?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql,
+                product.getProductName(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getDescription(),
+                product.getImageUrl(),
+                product.getStatus().name()
+                );
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(Long productId) {
+        String sql = "SELECT * FROM product WHERE product_id = ?";
+        List<Product> result = jdbcTemplate.query(sql, productRowMapper, productId);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Product> findAll() {
+        String sql = "SELECT * FROM product";
+        return jdbcTemplate.query(sql, productRowMapper);
+    }
+
+    @Override
+    public void update(Product product) {
+        String sql = "UPDATE product SET product_name = ?, price = ?, quantity = ?, description = ?, imageURL = ?, status =? WHERE product_id = ?";
+        jdbcTemplate.update(sql,
+                product.getProductName(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getDescription(),
+                product.getImageUrl(),
+                product.getStatus().name(),
+                product.getProductId()
+        );
+    }
+}
---
+++ src/main/java/com/myproject/ecommerce_service/repository/product/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.myproject.ecommerce_service.repository.product;
+
+import com.myproject.ecommerce_service.domain.product.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    Product registration(Product product); //product registration
+    Optional<Product> findById(Long productId); //Find by Product ID
+    List<Product> findAll(); // find all product
+    void update(Product product); //update product
+
+}
