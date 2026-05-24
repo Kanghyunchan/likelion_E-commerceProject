3b3913ef8a7a81a605bc2ea1f225b7d340114320
KangHyunChan <kanghyun122@naver.com>
2026-05-24 21:36:02 +0900

    refactor: orderCreateRequest DTO 수정

--- src/main/java/com/myproject/ecommerce_service/dto/order/OrderCreateRequest.java
+++ src/main/java/com/myproject/ecommerce_service/dto/order/OrderCreateRequest.java
@@ -11,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 public class OrderCreateRequest {
     private Long userId;
+    private String shippingAddress;
     private List<OrderItemElement> orderItems;
 
     @Getter
